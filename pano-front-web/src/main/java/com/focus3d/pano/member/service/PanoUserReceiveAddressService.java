d8debb89bc76c55c31f74231423c5e43ec32a0da
lihaijun <496174528@qq.com>
2017-08-25 17:10:44 +0800

    333

--- pano-front-web/src/main/java/com/focus3d/pano/member/service/PanoUserReceiveAddressService.java
+++ pano-front-web/src/main/java/com/focus3d/pano/member/service/PanoUserReceiveAddressService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import com.focus3d.pano.common.service.CommonService;
 /**
- * 
+ * 收获地址
  * *
  * @author lihaijun
  *
@@ -12,9 +12,11 @@ import com.focus3d.pano.common.service.CommonService;
  */
 public interface PanoUserReceiveAddressService<T> extends CommonService<T> {
 	/**
+	 * 获取用户收货地址
 	 * *
 	 * @param userSn
 	 * @return
 	 */
 	List<T> listByUser(long userSn);
+	
 }
