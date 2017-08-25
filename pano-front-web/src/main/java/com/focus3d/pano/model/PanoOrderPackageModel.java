c67fc2e22d8960557e9502bdf96c181660651b66
lihaijun <496174528@qq.com>
2017-08-25 15:07:14 +0800

    333333

---
+++ pano-front-web/src/main/java/com/focus3d/pano/model/PanoOrderPackageModel.java
@@ -0,0 +1,22 @@
+package com.focus3d.pano.model;
+
+import java.util.Date;
+
+import com.focus3d.pano.common.model.CommonModel;
+import com.focus3d.pano.model.ibator.PanoOrderPackageCriteria;
+import com.focus3d.pano.model.ibator.PanoOrderPackageDetail;
+/**
+ * 
+ * *
+ * @author lihaijun
+ *
+ */
+public class PanoOrderPackageModel extends PanoOrderPackageDetail<PanoOrderPackageModel, PanoOrderPackageCriteria> implements CommonModel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+}
