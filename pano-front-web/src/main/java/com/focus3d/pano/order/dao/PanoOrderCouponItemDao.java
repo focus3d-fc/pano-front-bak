584ebafc2f57e43bb08bc1a47cdf6c858bf0ef6f
lihaijun <496174528@qq.com>
2017-08-25 18:32:06 +0800

    333333

--- pano-front-web/src/main/java/com/focus3d/pano/order/dao/PanoOrderCouponItemDao.java
+++ pano-front-web/src/main/java/com/focus3d/pano/order/dao/PanoOrderCouponItemDao.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Repository;
 
 import com.focus3d.pano.common.dao.CommonDao;
 import com.focus3d.pano.model.PanoOrderCouponItemModel;
+import com.focus3d.pano.model.ibator.PanoOrderCouponItemCriteria;
 /**
  * 
  * *
@@ -12,5 +13,16 @@ import com.focus3d.pano.model.PanoOrderCouponItemModel;
  */
 @Repository
 public class PanoOrderCouponItemDao extends CommonDao<PanoOrderCouponItemModel> {
+	/**
+	 * 获取优惠券码
+	 * *
+	 * @param codeNum
+	 * @return
+	 */
+	public PanoOrderCouponItemModel getByCode(String codeNum) {
+		PanoOrderCouponItemCriteria criteria = new PanoOrderCouponItemCriteria();
+		criteria.createCriteria().andCodeNumEqualTo(codeNum);
+		return selectFirstByExample(criteria, PanoOrderCouponItemModel.class);
+	}
 
 }
--- pano-front-web/src/main/java/com/focus3d/pano/order/service/PanoOrderCouponItemService.java
+++ pano-front-web/src/main/java/com/focus3d/pano/order/service/PanoOrderCouponItemService.java
@@ -9,5 +9,11 @@ import com.focus3d.pano.common.service.CommonService;
  * @param <T>
  */
 public interface PanoOrderCouponItemService<T> extends CommonService<T> {
-
+	/**
+	 * 查找优惠券
+	 * *
+	 * @param code 优惠券码
+	 * @return
+	 */
+	T getByCode(String codeNum);
 }
