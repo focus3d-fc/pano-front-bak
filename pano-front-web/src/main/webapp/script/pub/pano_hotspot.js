7edfdfcdd003bb3de93305eaa60b47c3deb49af4
focus3d-fc <lihaijun@3d-focus.com>
2017-09-01 18:57:05 +0800

    2222

--- pano-front-web/src/main/webapp/script/pub/pano_hotspot.js
+++ pano-front-web/src/main/webapp/script/pub/pano_hotspot.js
@@ -112,7 +112,7 @@ function getHouse(){
 	    		var houseImg = data[i].houseImg;
 	    		var panoId = data[i].panoId;
 	    		 var img = $("<img/>").attr("src", houseImg).attr("pano_id",panoId).attr("encryptSn", encryptSn);
-	    		 $("<div/>").addClass("bd-r2").addClass("swiper-slide" + (i == 0 ? " bd-r " : "")).append($("<div/>").append($("<a/>").append(img).append($("<p/>").text(hosueName)))).appendTo($("#hx-swiper-wrapper"));
+	    		 $("<div/>").addClass("bd-r2").addClass("swiper-slide" + (i == 0 ? " bd-r " : "")).append($("<div/>").append(img).append($("<p/>").text(hosueName))).appendTo($("#hx-swiper-wrapper"));
 	    		//绑定点击户型图片事件，切换户型全景
 	    		img.bind("click", function(){
 	    			var houseSn = $(this).attr("encryptSn");
@@ -169,7 +169,7 @@ function getFjshow(panoId){
 	    		var sceneThumb = data[i].sceneThumb;
 	    		
 	    		var img = $("<img/>").attr("src", sceneThumb).attr("scene_id",sceneId);
-	    		 $("<div/>").addClass("bd-r2").addClass("swiper-slide" + (i == 0 ? " bd-r " : "")).append($("<div/>").append($("<a/>").append(img).append($("<p/>").text(sceneName)))).appendTo($("#hx-swiper-wrapper"));
+	    		 $("<div/>").addClass("bd-r2").addClass("swiper-slide" + (i == 0 ? " bd-r " : "")).append($("<div/>").append(img).append($("<p/>").text(sceneName))).appendTo($("#hx-swiper-wrapper"));
 	    		
 	    		//点击房间图片，切换场景
 	    		img.bind("click", function(){
